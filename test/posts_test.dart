9efcf55652d168256b3c967731db73921ce01d6e
Victoria Fisher <fisherv@oregonstate.edu>
2020-08-14 15:06:04 -0400

    Remove test from gitignore

---
+++ test/posts_test.dart
@@ -0,0 +1,26 @@
+import 'package:test/test.dart';
+import 'package:wasteagram/models/posts.dart';
+
+void main() {
+  test('Post should be successfully created', () {
+    DateTime testDate = DateTime.now();
+    String testURL = "NOIMAGEHERE.jpg";
+    double testLatitude = 1.0;
+    double testLongitude = 2.0;
+    int testQuantity = 5;
+
+    Post post = Post();
+
+    post.date = testDate;
+    post.imageURL = testURL;
+    post.latitude = testLatitude;
+    post.longitude = testLongitude;
+    post.quantity = testQuantity;
+
+    expect(post.date, testDate);
+    expect(post.imageURL, testURL);
+    expect(post.latitude, testLatitude);
+    expect(post.longitude, testLongitude);
+    expect(post.quantity, testQuantity);
+  });
+}
